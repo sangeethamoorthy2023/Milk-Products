e0b2f3617d589f8c9f9142e55cf2726d63ae8770
Vgoutham-2000 <145048734+Vgoutham-2000@users.noreply.github.com>
2023-10-18 14:12:41 +0530

    commit

--- milknook/app.py
+++ milknook/app.py
@@ -8,6 +8,7 @@ client = MongoClient('mongodb://localhost:27017/')
 db = client['test_db']
 collection = db['test_collection']
 
+#==========Home===============
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -18,14 +19,36 @@ def index():
 def product():
     return render_template('product.html')
 
+#=========About us=====================
+
+@app.route('/about')
+def about():
+    return render_template('About.html')
+
+#=========contact us====================
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+#==========Oreder now==================
+@app.route('/order')
+def order():
+    return render_template('order.html')
+
+#==========Login page======================
 @app.route('/open', methods=('GET', 'POST'))
 def login():
     return render_template('login.html')
 
-@app.route('/add', methods=['POST'])
+#===========Sign up=======================
+@app.route('/signup')
+def sign():
+    return render_template('signup.html')
+
+@app.route('/signupadd', methods=['POST'])
 def add():
     username = request.form['username']
-    #email = request.form['email']
+    email = request.form['email']
     password = request.form['password']
     
     existing_user = collection.find_one({'username': username})
@@ -34,7 +57,7 @@ def add():
         return 'User already exists!'
     else:
      
-        collection.insert_one({'username': username, 'password': password})
+        collection.insert_one({'username': username, 'password': password,'email':email})
         return render_template('login.html')
 
 
